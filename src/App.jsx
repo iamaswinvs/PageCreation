8b0aa2a912fefa349443ca8e59765b0c4cecf753
mr-nobody11 <aswinvs542@gmail.com>
2023-05-25 12:08:30 +0530

    pushing final draft of multiple pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import Navbar1 from "./components/Navbar";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <div>
+      <Navbar1/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
-
